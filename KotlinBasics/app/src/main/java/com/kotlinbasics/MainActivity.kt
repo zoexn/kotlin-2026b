b3042534231f1839779ca0961f7ef3198c136554
zoexn <kimmje06@gmail.com>
2026-09-14 11:35:42 +0900

    init

---
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.kotlinbasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kotlinbasics.ui.theme.KotlinBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinBasicsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        week03Variables()
+    }
+
+    private fun week03Variables() {
+        println("Week 03: Variables")
+
+        val courseName = "Mobile Programing" //java final
+        // courseName = "Data Structure" //error
+
+        var week = 2
+        week = 3
+        println("Course : $courseName")
+        println("Week : $week")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinBasicsTheme {
+        Greeting("Android")
+    }
+}
